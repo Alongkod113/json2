1245a8621dc916ea1c250693491ddf5716f6c4da
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-09 13:57:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,91 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Hello, world!</title>
     <style>
-        .bg{
-            background-color:green;
-        }
-        .bg2{
-            background-color:greenyellow;
-        }
-        .bg3{
-            background-color:gold;
+        .bg {
+            background-color: green;
         }
 
-</style>
+        .bg2 {
+            background-color: greenyellow;
+        }
+
+        .bg3 {
+            background-color: gold;
+            height: 1000px;
+            width: 300px;
+            float: left;
+        }
+    </style>
 
 </head>
-    <div class="container">
-        <h1>spotted tree</h1>
-        <img src="https://www.prachachat.net/wp-content/uploads/2021/09/%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%94%E0%B9%88%E0%B8%B2%E0%B8%87.jpg" alt="รูปภาพ" width="90%"><br>
-        <div class="col-01 bg3">[Float:left]</div>
+<div class="container">
+    <h1>spotted tree</h1>
+    <img src="https://www.prachachat.net/wp-content/uploads/2021/09/%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%94%E0%B9%88%E0%B8%B2%E0%B8%87.jpg"
+        alt="รูปภาพ" width="90%"><br>
+    <div class="col-12 bg3">[Float:left]</div>
     <div class="row bg">
         <div class="col-6 bg2">
             <br>
-            <img src="https://files.gqthailand.com/uploads/%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%81%E0%B8%A1%E0%B8%A3%E0%B8%81%E0%B8%951.jpg" alt="รูปภาพ" class="card-img-top"><br>
+            <img src="https://files.gqthailand.com/uploads/%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%81%E0%B8%A1%E0%B8%A3%E0%B8%81%E0%B8%951.jpg"
+                alt="รูปภาพ" class="card-img-top"><br>
             <div class="card-body">
-                <p class="card-text">กวักมรกตขึ้นชื่อว่าเป็นไม้ในร่มที่เลี้ยงง่าย ปกติจะมีใบอวบน้ำสีเขียวมรกต แต่สำหรับพันธุ์ด่างจะมีใบบางส่วนเป็นสีเหลืองหรือขาวนวลทั้งใบหรือขึ้นเป็นจุดแซมกับใบสีเขียว กวักมรกตไม่ชอบแสงแดด แต่ควรเลี้ยงในห้องที่มีอากาศถ่ายเทและมีแสงส่องรำไร รดน้ำแค่สัปดาห์ละสองครั้งพอ หากรดมากเกินไปจนดินแฉะจะทำให้รากเน่าได้ ความยากในการเลี้ยงกวักมรกตด่างคือเราจะสังเกตว่าใบซีดขาวนั้นเป็นธรรมชาติหรือใบมีปัญหากันแน่</p>
+                <p class="card-text">กวักมรกตขึ้นชื่อว่าเป็นไม้ในร่มที่เลี้ยงง่าย ปกติจะมีใบอวบน้ำสีเขียวมรกต
+                    แต่สำหรับพันธุ์ด่างจะมีใบบางส่วนเป็นสีเหลืองหรือขาวนวลทั้งใบหรือขึ้นเป็นจุดแซมกับใบสีเขียว
+                    กวักมรกตไม่ชอบแสงแดด แต่ควรเลี้ยงในห้องที่มีอากาศถ่ายเทและมีแสงส่องรำไร รดน้ำแค่สัปดาห์ละสองครั้งพอ
+                    หากรดมากเกินไปจนดินแฉะจะทำให้รากเน่าได้
+                    ความยากในการเลี้ยงกวักมรกตด่างคือเราจะสังเกตว่าใบซีดขาวนั้นเป็นธรรมชาติหรือใบมีปัญหากันแน่</p>
+            </div>
+            <div>
+            </div>
+            <div class="col-6 bg">
+                <br>
+                <img src="https://files.gqthailand.com/uploads/Pink-Princess1.jpg" alt="รูปภาพ" class="card-img-top"
+                    alt="รูปภาพ"><br>
+                <div class="card-body"></div>
+                <p class="card-text">ฟิโลเดนดรอนเป็นพรรณไม้ธรรมดาสำหรับเลี้ยงที่บ้าน ราคาไม่แพง ดูแลง่าย ใบมีรูปทรงสวย
+                    แต่ต้นที่ญาญ่า - อุรัสยา เสปอร์บันด์ นำมาเลี้ยงจนเป็นผู้นำเทรนด์ (จนราคาพุ่งไปสูงลิ่ว)
+                    คือฟิโลเดนดรอน พิงค์ ปรินเซส ที่ใบสีเขียวจะมีสีชมพูแทรกเข้ามาครึ่งใบบ้าง ค่อนใบบ้าง
+                    เป็นไม้ที่ปลูกไว้ในบ้านได้ มีอากาศถ่ายเท แสงส่องรำไร รดน้ำปานกลาง
+                    แต่อย่างที่น้องญาญ่าบอกว่าหากเลี้ยงไว้ในที่โดนแสงสีชมพูสดบนใบก็จะซีดขาว จริงๆ
+                    ฟิโลเดนดรอนมีพันธุ์ด่างอีกเยอะมาก อาทิ ฟิโลเดนดรอนมะละกอด่าง ฟิโลเดนดรอน ไวท์ไนท์
+                    ฟิโลเดนดรอนก้ามกุ้งด่าง </p>
+            </div>
         </div>
-    <div>
+        <div class="col-12 bg0">
+            <div id="main">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th> Details </th>
+                        </tr>
+                    </thead>
+                    <tbody id="tblPosts">
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<br>
+<div>
+    <div class="col-12 bg0 center">
+        <h1> 63110282 อลงกต ธุระวงศ์</h1>
+    </div>
+</div>
+</body>
 
-    </div>
-    <div class="col-02 bg">
-            <br>
-            <img src="https://files.gqthailand.com/uploads/Pink-Princess1.jpg" alt="รูปภาพ"class="card-img-top" alt="รูปภาพ" ><br>
-            <div class="card-body"></div>
-                <p  class="card-text">ฟิโลเดนดรอนเป็นพรรณไม้ธรรมดาสำหรับเลี้ยงที่บ้าน ราคาไม่แพง ดูแลง่าย ใบมีรูปทรงสวย แต่ต้นที่ญาญ่า - อุรัสยา เสปอร์บันด์ นำมาเลี้ยงจนเป็นผู้นำเทรนด์ (จนราคาพุ่งไปสูงลิ่ว) คือฟิโลเดนดรอน พิงค์ ปรินเซส ที่ใบสีเขียวจะมีสีชมพูแทรกเข้ามาครึ่งใบบ้าง ค่อนใบบ้าง เป็นไม้ที่ปลูกไว้ในบ้านได้ มีอากาศถ่ายเท แสงส่องรำไร รดน้ำปานกลาง แต่อย่างที่น้องญาญ่าบอกว่าหากเลี้ยงไว้ในที่โดนแสงสีชมพูสดบนใบก็จะซีดขาว จริงๆ ฟิโลเดนดรอนมีพันธุ์ด่างอีกเยอะมาก อาทิ ฟิโลเดนดรอนมะละกอด่าง ฟิโลเดนดรอน ไวท์ไนท์ ฟิโลเดนดรอนก้ามกุ้งด่าง </p>
-        </div>
-    </div>
+</html>
