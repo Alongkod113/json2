b0e4f29f79feda772ee25b99b1836bdff9a170d5
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-09 12:34:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,179 +1,63 @@
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Json2</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Hello, world!</title>
 </head>
+<style>
+    .bg1 {
+        background-color: red;
+    }
+
+    .bg2 {
+        background-color: green;
+    }
+
+    .bg3 {
+        background-color: blue;
+        width: 200px;
+        height: 500px;
+        float: left;
+    }
+
+    .bg4 {
+        background-color: darkkhaki;
+    }
+</style>
+
 <body>
-    <button id="btnBack"> back </button>
-    <div id="main">
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                </tr>
-            </thead>
-            <tbody id="tblPost">
-            </tbody>
-        </table>
+    <div class="container">
+        <img src="https://s3-ap-southeast-1.amazonaws.com/media.storylog/storycontent/5ad57030f0abf15642a597b5/15268670418467142123.jpg"
+            class="img-fluid" alt="..." width="75%">
+        <div class="container">
+            <div class="col-12 bg3">[Float:left]</div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://sites.google.com/site/planetinworld2558/_/rsrc/1445078020159/home/daw-phuth-mercury/%E0%B8%9E%E0%B8%B8%E0%B8%98.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">ดาวพุธเป็นดาวเคราะห์ที่อยู่ใกล้ดวงอาทิตย์มากที่สุด และเป็นดาวเคราะห์ที่เล็กที่สุดในระบบสุริยะ ใช้เวลาโคจรรอบดวงอาทิตย์ 87.969 วัน</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://sites.google.com/site/gatetouni/_/rsrc/1473238042961/solarsystem/earth/solar41.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">เป็นดาวเคราะห์ลำดับที่สามจากดวงอาทิตย์ และเป็นวัตถุทางดาราศาสตร์เพียงหนึ่งเดียวที่ทราบว่ามีสิ่งมีชีวิต จากการวัดอายุด้วยกัมมันตรังสีและแหล่งหลักฐานอื่นได้ความว่าโลกกำเนิดเมื่อประมาณ 4,500 ล้านปี</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">.col-6<br>Subsequent columns continue along the new line.</div>
+        </div>
     </div>
-    <div id="detail">
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>UserID</th>
-                </tr>
-            </thead>
-            <tbody id="tbldetails">
-            </tbody>
-        </table>
-    </div>
-    <div id="main2">
-        <table>
-            <thead>
-                <th>postID</th><br/>
-                <th>id</th>
-                <th>name</th>
-                <th>email</th>
-                <th>body</th>
-
-            </thead>
-            <tbody id="tblPost2">
-            </tbody>
-        </table>
-    </div>
-    <div id="comment">
-        <table>
-            <thead>
-                 <tr>
-                  <th>postId</th>
-  
-                  <th>id</th>
-                  <th>name</th>
-                  <th>email</th>
-                  <th>body</th>
-                </tr>
-            </thead>
-            <tbody id="tblcommets">
-            </tbody>
-        </table>
-    </div>
-    
 </body>
-<script>
-    function LoadPosts() {
-        $("#main").show(1-9);
-        $("#detail").hide();
-        $("main2").hide();
-        var url = "https://jsonplaceholder.typicode.com/posts"
-        $.getJSON(url)
-            .done((data) => {
-                $.each(data, (k, item) => {
-                    
-                    var line = "<tr>";
-                    line += "<td>" + item.id + "</td>"
-                    line += "<td><b>" + item.title + "</b><br/>"
-                    line += item.body + "</td>"
-                    line += "<td><button onClick='showDetails(" + item.id + ");'>Link</button></td>"
-                    line += "</tr>";
-                    $("#tblPost").append(line);
-                });
-                $("#main").show(1-9);
-            })
-            .fail((xhr, err, status) => {
-            })
-    }
-    
-    function showDetails(id) {
-        $("#main").hide();
-        $("#detail").show();
-        $("main2").hide();
-        var url = "https://jsonplaceholder.typicode.com/posts/" + id
-        $.getJSON(url)
-            .done((data) => {
-                console.log(data);
-                var line = "<tr id='details'";
-                    line += "><td>" + data.id + "</td>"
-                    line += "<td><b>" + data.title + "</b><br/>"
-                    line += data.body + "</td>"
-                    line += "<td>" + data.userId + "</td>"
-                    line += "</tr>";
-                    $("#tbldetails").append(line);
-             
-            })
-            .fail((xhr, err, status) => {
-            })
-    }
 
-    function LoadPosts2() {
-        $("#main").hide();
-        $("#comments").hide();
-        $("main2").show();
-        var url = "https://jsonplaceholder.typicode.com/comments"
-        $.getJSON(url)
-            .done((data) => {
-              $.each(data, (k, item) => {
-                    var line = "<tr>";
-                    line += "<td>" + item.postID + "</td>"
-                    line += "<td><b>" + item.id + "</b><br/>"
-                    line += "<td><b>" + item.name+ "</b><br/>"
-                    line += "<td><b>" + item.email + "</b><br/>"
-                    line -=  item.body+ "</td>"
-                    line += "</tr>";
-                    $("#tblPost2").append(line);
-                });
-                $("#main").show();
-            })
-            .fail((xhr, err, status) => {
-            })
-    }
-
-    function showcomments(id) {
-        $("#main").hide();
-        $("#commsnts").show();
-        $("#main2").show();
-        var url = "https://jsonplaceholder.typicode.com/posts/"+id+"/comments"
-        $.getJSON(url)
-            .done((data) => {
-                console.log(data);
-                var line = "<tr id='comments'";
-                 
-                    line += "<td><b>" + data.id + "</b><br/>"
-                    line += "<td><b>" + data.name + "</b><br/>"
-                    line += "<td><b>" + data.email + "</b><br/>"
-                    line += "<td><b>" + data.body + "</b><br/>"
-                    /*line += data.body + "</td>"*/
-                    line += "<td>" + data.postId+ "</td>"
-                    line += "</tr>";
-                    $("#tblcomment").append(line);
-             
-            })
-            .fail((xhr, err, status) => {
-            })
-
-          }
-          
-    $(() => {
-        LoadPosts();
-
-        $("#detail").hide();
-        
-            $("#main").show(1-9);
-            $("#btnBack").click(() => {
-            $("#main").show(1-9);
-            $("#details").remove(1-9);
-        });
-        LoadPosts2();
-    })
-  
-</script>
 </html>
