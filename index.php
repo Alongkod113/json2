d4f7a50b419904363831f2cf121537bebbf0e677
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-09 14:44:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,76 +10,100 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>63112379_Alongkod</title>
     <style>
         .bg {
             background-color: green;
         }
 
-        .bg2 {
+        .bg0 {
             background-color: greenyellow;
         }
 
-        .bg3 {
+        .bg1 {
             background-color: gold;
         }
-</style>
+
+        .bg2 {
+            background-color: gray;
+        }
+
+        .bg3 {
+            background-color: ghostwhite;
+        }
+    </style>
 </head>
+
 <body class="bg">
-  <div class="container">
-      <div>
-          <div class="col-12 bg0">
-              <img src="https://www.smeleader.com/wp-content/uploads/2020/03/%E0%B8%95%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%82%E0%B8%99%E0%B8%A1-%E0%B8%95%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B9%80%E0%B8%9A%E0%B9%80%E0%B8%81%E0%B8%AD%E0%B8%A3%E0%B8%B5%E0%B9%88-660x330.jpg" class="rounded mx-auto d-block" alt=" ">
-          </div>
-      </div>
-      <br>
-      <div class="row">
-          <div class="col-4 bg1">
-          
-          </div>
-          <br>
-          <div class="col-8 bg2">
-              <div class="row">
-                  <div class="col-6 ">
-                      <picture>
-                          <source srcset="https://s359.kapook.com/pagebuilder/735689c8-45f3-43a7-a4e4-762352982580.jpg" type="image/svg+xml">
-                          <img src="https://s359.kapook.com/pagebuilder/735689c8-45f3-43a7-a4e4-762352982580.jpg" class="img-fluid img-thumbnail" alt=" ">
-                      </picture>
-                      <p class="card-text">เบเกอรี่เริ่มมีขึ้นในยุคหิน โดยชาวสวิสได้เป็นผู้ริเริ่มนำเมล็ดข้าวสาลีมาบดให้แตก ผสมน้ำ ทำให้สุกบนแผ่นหินเผาไฟ ได้อาหารเป็นแผ่น ข้างในเหนียวเหนอะหนะ นับเป็นขนมปังชนิดแรกของโลกเลยค่ะ และต่อมา ชาวอียิปต์ได้พัฒนาจากขนมปังที่เป็นก้อนแน่น ให้มาเป็นก้นโปร่งฟูขึ้น ซึ่งมาจากที่ชาวอียิปต์หมักก้อนแป้งแล้วลืมทิ้งไว้  และได้นำมาผสมกับแป้งที่ทำใหม่เพื่อให้ขนมขึ้นฟู และชาวอียิปต์ยังได้นำดินเหนียวมาทำเป็นภาชนะเพื่อใช้ในการอบขนมแทนแผ่นหิน ซึ่งนั่นก็ถือว่าเป็นเตาอบชนิดแรกของโลก และเตาอบชนิดนี้ แบ่งเป็น 2 ชั้น คือ ชั้นล่างไว้ก่อไฟ ชั้นบนสำหรับอบขนม  เชื่อกันว่าเริ่มมีการผลิตมาตั้งแต่ ๓,๐๐๐ ปีก่อน  โดยต้นกำเนิดมาจากพวกทาสในยุคอียิปต์โบราณ ที่ได้ผสมก้อนแป้งที่ลืมทิ้งไว้ลงในแป้งที่ผสมเสร็จใหม่ๆ ทำให้เกิดผลิตภัณฑ์ที่มีลักษณะคล้ายขนมปัง คือเนื้อแป้งเบา ฟู และมีรสชาติอร่อย หลังจากนำไปทำให้สุก </p>
-                  </div>
-                  <div class="col-6 ">
-                      <picture>
-                          <source srcset="https://www.ryt9.com/img/files/20190226/iq8f8507d0b550368320ff0c887dabda26-0.jpg" type="image/svg+xml">
-                          <img src="https://www.ryt9.com/img/files/20190226/iq8f8507d0b550368320ff0c887dabda26-0.jpg" class="img-fluid img-thumbnail" alt="...">
-                      </picture>
-                      <p class="card-text">ในสมัยกรีกได้พัฒนาการทำขนมปัง โดยปั้นเป็นก้อนกลมรี น้ำหนักก้อนละ 1 ปอนด์ และเปลี่ยนรูปแบบเตาอบเป็นลักษณะคล้ายรวงผื้ง ซื่งยังใช้ไม้เป็นเชื้อเพลิงเหมือนเดิม  ต่อมาในสมัยโรมัน ก็มีการพัฒนาเทคโนโลยีการทำขนมปังเพิ่มขึ้น โดยสร้างเครื่องผสมซึ่งประกอบด้วยอ่างหินและพายไม้ และก็พัฒนามาเรื่อย จนถึงศตวรรษที่ 13 ชาวฝรั่งเศสได้บันทึกถึงความก้าวหน้าของการพัฒนาเครื่องทำขนม ชนิดของขนมปัง และเมื่อกลางศตวรรษที่ 20 เป็นต้นมาอุตสาหกรรมขนมอบก็เริ่มเกิดขึ้น และมีการพัฒนาต่อเนื่อง</p>
-                  </div>
-                  <div class="col-12 bg0">
-                      <div id="main">
-                          <table>
-                              <thead>
-                                  <tr>
-                                      <th>ID</th> <th>Title</th><th> Details </th>
-                                  </tr>
-                              </thead>
-                              <tbody id="tblPosts">
-                              </tbody>
-                          </table>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <br>
-      <div>
-          <div class="col-12 bg0 center">
-              <h1>ศักดิ์ดา เอี่ยมพงศ์ษา 63110282</h1>
-          </div>
-      </div>
-      
+    <div class="container">
+        <div>
+            <div class="col-12 bg0">
+                <img src="https://www.prachachat.net/wp-content/uploads/2021/09/%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%94%E0%B9%88%E0%B8%B2%E0%B8%87.jpg"
+                    class="rounded mx-auto d-block" alt="รูปภาพ">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-4 bg1">
 
-  </div>
+            </div>
+            <br>
+            <div class="col-8 bg2">
+                <div class="row">
+                    <div class="col-6 ">
+                        <picture>
+                            <img src="https://files.gqthailand.com/uploads/%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%81%E0%B8%A1%E0%B8%A3%E0%B8%81%E0%B8%951.jpg"
+                                class="img-fluid img-thumbnail" alt="รูปภาพ">
+                        </picture>
+                        <p class="card-text">กวักมรกตขึ้นชื่อว่าเป็นไม้ในร่มที่เลี้ยงง่าย ปกติจะมีใบอวบน้ำสีเขียวมรกต
+                            แต่สำหรับพันธุ์ด่างจะมีใบบางส่วนเป็นสีเหลืองหรือขาวนวลทั้งใบหรือขึ้นเป็นจุดแซมกับใบสีเขียว
+                            กวักมรกตไม่ชอบแสงแดด แต่ควรเลี้ยงในห้องที่มีอากาศถ่ายเทและมีแสงส่องรำไร
+                            รดน้ำแค่สัปดาห์ละสองครั้งพอหากรดมากเกินไปจนดินแฉะจะทำให้รากเน่าได้
+                            ความยากในการเลี้ยงกวักมรกตด่างคือเราจะสังเกตว่าใบซีดขาวนั้นเป็นธรรมชาติหรือใบมีปัญหากันแน่
+                        </p>
+                    </div>
+                    <div class="col-6 ">
+                        <picture>
+                            <img src="https://files.gqthailand.com/uploads/Pink-Princess1.jpg"
+                                class="img-fluid img-thumbnail" alt="รูปภาพ">
+                        </picture>
+                        <p class="card-text">ฟิโลเดนดรอนเป็นพรรณไม้ธรรมดาสำหรับเลี้ยงที่บ้าน ราคาไม่แพง ดูแลง่าย
+                            ใบมีรูปทรงสวยแต่ต้นที่ญาญ่า - อุรัสยา เสปอร์บันด์ นำมาเลี้ยงจนเป็นผู้นำเทรนด์
+                            (จนราคาพุ่งไปสูงลิ่ว)
+                            คือฟิโลเดนดรอน พิงค์ ปรินเซส ที่ใบสีเขียวจะมีสีชมพูแทรกเข้ามาครึ่งใบบ้าง ค่อนใบบ้าง
+                            เป็นไม้ที่ปลูกไว้ในบ้านได้ มีอากาศถ่ายเท แสงส่องรำไร รดน้ำปานกลาง
+                            แต่อย่างที่น้องญาญ่าบอกว่าหากเลี้ยงไว้ในที่โดนแสงสีชมพูสดบนใบก็จะซีดขาว จริงๆ
+                            ฟิโลเดนดรอนมีพันธุ์ด่างอีกเยอะมาก อาทิ ฟิโลเดนดรอนมะละกอด่าง ฟิโลเดนดรอน ไวท์ไนท์
+                            ฟิโลเดนดรอนก้ามกุ้งด่าง</p>
+                    </div>
+                    <div class="col-12 bg0">
+                        <div id="main">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Title</th>
+                                        <th> Details </th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tblPosts">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div>
+            <div class="col-12 bg0 center">
+                <h1>63112379_Alongkod</h1>
+            </div>
+        </div>
 
-  
+
+    </div>
+
 </body>
+
 </html>
