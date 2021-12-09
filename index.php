11ceb4b97205926cff8dc3f9688520a8ebd911e4
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-09 12:49:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,63 +1,55 @@
+<!doctype html>
 <html lang="en">
-
-<head>
+  <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <title>Hello, world!</title>
-</head>
-<style>
-    .bg1 {
-        background-color: red;
-    }
+    <style>
+        .bg{
+            background-color:green;
+        }
+        .bg2{
+            background-color:blue;
+        }
+        .bg3{
+            background-color:red;
+            height: 600px;
+            width: 300px;
+            float: left;
+        }
 
-    .bg2 {
-        background-color: green;
-    }
-
-    .bg3 {
-        background-color: blue;
-        width: 200px;
-        height: 500px;
-        float: left;
-    }
-
-    .bg4 {
-        background-color: darkkhaki;
-    }
 </style>
 
-<body>
+</head>
     <div class="container">
-        <img src="https://s3-ap-southeast-1.amazonaws.com/media.storylog/storycontent/5ad57030f0abf15642a597b5/15268670418467142123.jpg"
-            class="img-fluid" alt="..." width="75%">
-        <div class="container">
-            <div class="col-12 bg3">[Float:left]</div>
+        <img src="https://www.prachachat.net/wp-content/uploads/2021/09/%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%94%E0%B9%88%E0%B8%B2%E0%B8%87.jpg" alt="รูปภาพ">
+        
+        <h1>Hello</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio minus quos molestiae, beatae assumenda ipsam iure laudantium vero itaque quisquam iusto amet possimus quis ipsa dignissimos culpa tempora nobis ducimus.</p>
+<hr>
+    <div class="row bg">
+        <div class="col-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, necessitatibus reiciendis distinctio in commodi quis ab repudiandae aspernatur dolore expedita, laboriosam dolor maiores accusamus amet sequi aperiam unde minus iste.
         </div>
-        <div class="row">
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://sites.google.com/site/planetinworld2558/_/rsrc/1445078020159/home/daw-phuth-mercury/%E0%B8%9E%E0%B8%B8%E0%B8%98.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">ดาวพุธเป็นดาวเคราะห์ที่อยู่ใกล้ดวงอาทิตย์มากที่สุด และเป็นดาวเคราะห์ที่เล็กที่สุดในระบบสุริยะ ใช้เวลาโคจรรอบดวงอาทิตย์ 87.969 วัน</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://sites.google.com/site/gatetouni/_/rsrc/1473238042961/solarsystem/earth/solar41.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">เป็นดาวเคราะห์ลำดับที่สามจากดวงอาทิตย์ และเป็นวัตถุทางดาราศาสตร์เพียงหนึ่งเดียวที่ทราบว่ามีสิ่งมีชีวิต จากการวัดอายุด้วยกัมมันตรังสีและแหล่งหลักฐานอื่นได้ความว่าโลกกำเนิดเมื่อประมาณ 4,500 ล้านปี</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-9">.col-6<br>Subsequent columns continue along the new line.</div>
+
+        <div class="col-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, necessitatibus reiciendis distinctio in commodi quis ab repudiandae aspernatur dolore expedita, laboriosam dolor maiores accusamus amet sequi aperiam unde minus iste.
         </div>
     </div>
-</body>
+<hr>
+    <div class="">
+        <div class="col-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, necessitatibus reiciendis distinctio in commodi quis ab repudiandae aspernatur dolore expedita, laboriosam dolor maiores accusamus amet sequi aperiam unde minus iste.
+        </div>
 
+        <div class="col-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, necessitatibus reiciendis distinctio in commodi quis ab repudiandae aspernatur dolore expedita, laboriosam dolor maiores accusamus amet sequi aperiam unde minus iste.
+        </div>
+    </div>
+  </body>
 </html>
