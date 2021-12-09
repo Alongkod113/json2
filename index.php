cc8fbce1f093973d2a534e140fbf30957ff1f3c9
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-10 00:30:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,96 +4,196 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.js"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Hello, world!</title>
+
+    <title>63112379_Alongkod</title>
+    <style>
+        .bg {
+            background-color: green;
+        }
+
+        .bg0 {
+            background-color: greenyellow;
+        }
+
+        .bg1 {
+            background-color: gold;
+        }
+
+        .bg2 {
+            background-color: gray;
+        }
+
+        .bg3 {
+            background-color: ghostwhite;
+        }
+    </style>
 </head>
-<style>
-    .bg1 {
-        background-color: red;
-    }
 
-    .bg2 {
-        background-color: green;
-    }
-
-    .bg3 {
-        background-color: blue;
-        width: 200px;
-        height: 1270px;
-        float: left;
-    }
-
-    .bg4 {
-        background-color: darkkhaki;
-        text-align: center;
-        width: 80%
-    }
-</style>
-
-<body>
+<body class="bg">
     <div class="container">
-        <img src="https://s3-ap-southeast-1.amazonaws.com/media.storylog/storycontent/5ad57030f0abf15642a597b5/15268670418467142123.jpg"
-            class="img-fluid" alt="..." width="80%">
-        <div class="container">
-            <div class="col-12 bg3">[Float:left]</div>
-        </div>
-        <div class="row">
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://sites.google.com/site/planetinworld2558/_/rsrc/1445078020159/home/daw-phuth-mercury/%E0%B8%9E%E0%B8%B8%E0%B8%98.jpg"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">ดาวพุธเป็นดาวเคราะห์ที่อยู่ใกล้ดวงอาทิตย์มากที่สุด
-                            และเป็นดาวเคราะห์ที่เล็กที่สุดในระบบสุริยะ ใช้เวลาโคจรรอบดวงอาทิตย์ 87.969 วัน</p>
-                    </div>
-                </div>
+        <div>
+            <div class="col-12 bg0">
+                <img src="https://www.prachachat.net/wp-content/uploads/2021/09/%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%94%E0%B9%88%E0%B8%B2%E0%B8%87.jpg"
+                    class="rounded mx-auto d-block" alt="รูปภาพ">
             </div>
-            <div class="col-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://sites.google.com/site/gatetouni/_/rsrc/1473238042961/solarsystem/earth/solar41.jpg"
-                        class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">เป็นดาวเคราะห์ลำดับที่สามจากดวงอาทิตย์
-                            และเป็นวัตถุทางดาราศาสตร์เพียงหนึ่งเดียวที่ทราบว่ามีสิ่งมีชีวิต
-                            จากการวัดอายุด้วยกัมมันตรังสีและแหล่งหลักฐานอื่นได้ความว่าโลกกำเนิดเมื่อประมาณ 4,500 ล้านปี
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-4 bg1">
+
+            </div>
+            <br>
+            <div class="col-8 bg2">
+                <div class="row">
+                    <div class="col-6 ">
+                        <picture>
+                            <img src="https://files.gqthailand.com/uploads/%E0%B8%81%E0%B8%A7%E0%B8%B1%E0%B8%81%E0%B8%A1%E0%B8%A3%E0%B8%81%E0%B8%951.jpg"
+                                class="img-fluid img-thumbnail" alt="รูปภาพ">
+                        </picture>
+                        <p class="card-text">กวักมรกตขึ้นชื่อว่าเป็นไม้ในร่มที่เลี้ยงง่าย ปกติจะมีใบอวบน้ำสีเขียวมรกต
+                            แต่สำหรับพันธุ์ด่างจะมีใบบางส่วนเป็นสีเหลืองหรือขาวนวลทั้งใบหรือขึ้นเป็นจุดแซมกับใบสีเขียว
+                            กวักมรกตไม่ชอบแสงแดด แต่ควรเลี้ยงในห้องที่มีอากาศถ่ายเทและมีแสงส่องรำไร
+                            รดน้ำแค่สัปดาห์ละสองครั้งพอหากรดมากเกินไปจนดินแฉะจะทำให้รากเน่าได้
+                            ความยากในการเลี้ยงกวักมรกตด่างคือเราจะสังเกตว่าใบซีดขาวนั้นเป็นธรรมชาติหรือใบมีปัญหากันแน่
                         </p>
                     </div>
+                    <div class="col-6 ">
+                        <picture>
+                            <img src="https://files.gqthailand.com/uploads/Pink-Princess1.jpg"
+                                class="img-fluid img-thumbnail" alt="รูปภาพ">
+                        </picture>
+                        <p class="card-text">ฟิโลเดนดรอนเป็นพรรณไม้ธรรมดาสำหรับเลี้ยงที่บ้าน ราคาไม่แพง ดูแลง่าย
+                            ใบมีรูปทรงสวยแต่ต้นที่ญาญ่า - อุรัสยา เสปอร์บันด์ นำมาเลี้ยงจนเป็นผู้นำเทรนด์
+                            (จนราคาพุ่งไปสูงลิ่ว)
+                            คือฟิโลเดนดรอน พิงค์ ปรินเซส ที่ใบสีเขียวจะมีสีชมพูแทรกเข้ามาครึ่งใบบ้าง ค่อนใบบ้าง
+                            เป็นไม้ที่ปลูกไว้ในบ้านได้ มีอากาศถ่ายเท แสงส่องรำไร รดน้ำปานกลาง
+                            แต่อย่างที่น้องญาญ่าบอกว่าหากเลี้ยงไว้ในที่โดนแสงสีชมพูสดบนใบก็จะซีดขาว จริงๆ
+                            ฟิโลเดนดรอนมีพันธุ์ด่างอีกเยอะมาก อาทิ ฟิโลเดนดรอนมะละกอด่าง ฟิโลเดนดรอน ไวท์ไนท์
+                            ฟิโลเดนดรอนก้ามกุ้งด่าง</p>
+                    </div>
+                    <div class="col-12 bg0">
+                        <div id="main">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Title</th>
+                                        <th> Details </th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tblPosts">
+                                </tbody>
+                                </head>
+
+                                <body>
+                                    <button id="btnBack"> back </button>
+                                    <div id="main">
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>Title</th>
+                                                    <th> Details </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="tblPosts">
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                    <div id="Detail">
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>Title</th>
+                                                    <th>Userid</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="tblDetails">
+                                            </tbody>
+                                        </table>
+                                    </div>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="col-9">
-                <button id="btnBack"> back </button>
+        </div>
+        <br>
+        <div>
+            <div class="col-12 bg0 center">
+                <h1>63112379_Alongkod</h1>
+            </div>
+        </div>
 
-                <div id="main">
-                    <table  class="table table-success table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Author</th>
-                            </tr>
-                        </thead>
-                        <tbody id="tblPost">
-                        </tbody>
-                    </table>
-                </div>
 
-                <div id="detail">
-                    <table class="table table-success table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>UserID</th>
-                            </tr>
-                        </thead>
-                        <tbody id="tblDetails">
-                        </tbody>
-                    </table>
-                </div>
+    </div>
+</body>
+<script>
+    function showDetails(id) {
+        $("#main").hide();
+        $("#etail").show();
+        // console.log(id);
+        var url = "https://jsonplaceholder.typicode.com/posts/" + id;
+        $.getJSON(url)
+            .done((data) => {
+                console.log(data);
+                var line = "<tr id='detailROW'";
+                line += "><td>" + data.id + "</td>"
+                line += "<td><b>" + data.title + "</b><br/>"
+                line += data.body + "</td>"
+                line += "<td>" + data.userId + "</td>"
+                line += "</tr>";
+                $("#tblDetails").append(line);
+            })
+            .fail((xhr, status, error) => {
+            })
+    }
+
+    function loadPosts() {
+        var url = "https://jsonplaceholder.typicode.com/posts"
+        var i = 0;
+        $.getJSON(url)
+            .done((data) => {
+                $.each(data, (k, item) => {
+                    // console.log(item);
+                    i++;
+                    var line = "<tr>";
+                    line += "<td>" + item.id + "</td>";
+                    line += "<td><b>" + item.title + "</b><br/>";
+                    line += item.body + "</td>";
+                    line += "<td> <button onClick='showDetails(" + item.id + ");' > link </button> </td>";
+                    line += "</tr>";
+                    $("#tblPosts").append(line);
+                    if (i == 10) {
+                        loadPosts().stop();
+                    };
+                });
+                $("#main").show();
+            })
+            .fail((xhr, status, error) => {
+            })
+    }
+
+
+    $(() => {
+        loadPosts();
+        $("#detail").hide();
+        $("#btnBack").click(() => {
+            $("#main").show();
+            $("#detail").hide();
+            $("#detailROW").remove();
+        });
+    })
+</script>
+
+
+</html>
