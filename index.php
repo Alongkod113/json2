ab0a03be03785d049aae056cef269a5e38d9c731
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-14 13:36:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,29 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-    crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <title>Document</title>
-</head>
+</head><script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <body>
-    
-</body>
-<div class="container">
-    <form action="">
-      <div class="form-group">
-        <label for="frmEmail"> Username </label>
-        <input type="email"  id="frmEmail" class="form-control" required />
-      </div>
-      <div class="form-group">
-        <label for="frmPassword">Password</label>
-        <input type="password"  id="frmPassword" class="form-control" required />
-      </div>
-      <input type="submit" value="Send"  class="btn btn-primary" />
-    </form>
-  
-</div>
+    Alongkod
+    <h1>line 1</h1>
+    <h1>line 2</h1>
+    <h1 id="test">line 3</h1>
+    <button id="btn1"> Me </button>
+    <button id="btn2"> du </button>
 
+</body>
+
+<script>
+
+    function test_click(){
+        $("#test").Me(); // .Show()
+    }
+    function test_click(){
+        $("#test").du();
+    }
+    $(()=>{
+        console.log("start")
+        $("#Btn1").click(test_click); //register byn1 for clik
+        $("#Btn2").click(test_click)
+    });
+  
+    
+
+</script>
 </html>
