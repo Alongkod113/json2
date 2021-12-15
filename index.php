41c6e0ea5818496de5e20dd6e7b92865d6e3f15d
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-15 22:02:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,21 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>63112379_Alongkod</title>
+    <title>Document</title>
 </head>
 
 <body>
-    <div class="col-input my-input"><input type="number" name="fUser"></div>
-    
+
 </body>
-</html>
+<div class="container">
+    <form action="">
+        <div class="form-group">
+            <label for="frmEmail"> Username </label>
+            <input type="email" id="frmEmail" class="form-control" required />
+        </div>
+        <div class="form-group">
+            <label for="frmPassword">Password</label>
+            <input type="password" id="frmPassword" class="form-control" required />
+        </div>
+        <input type="submit" value="Send" class="btn btn-primary" />
+    </form>
